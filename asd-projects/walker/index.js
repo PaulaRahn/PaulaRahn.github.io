d7606b82a122bb0c933f7e525d41e7b6196a3cc3
PaulaRahn <paula.rahn@apsb.org>
2022-12-09 20:07:39 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,25 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    "LEFT": 37,
+    "UP": 38,
+    "RIGHT": 39,
+    "DOWN": 40
+  }
+
+  var positionX = 0;
+  var positionY = 0;
+  var speedX = 0;
+  var speedY = 0;
+
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,15 +39,38 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem();
+    redrawGameItem();
 
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    //console.log(event.which);
 
+    if (event.which === 37) {
+      //console.log("left pressed");
+      speedY = -5;
+    }
+    else if (event.which === 38) {
+      //console.log("up pressed");
+      speedX = -5;
+    }
+    else if (event.which === 39) {
+      //console.log("right pressed");
+      speedY = 5;
+    }
+    else if (event.which === 40) {
+      //console.log("down pressed");
+      speedX = 5;
+    }
+  }
+
+  function handleKeyUp(event) {
+    speedX = 0;
+    speedY = 0;
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -49,6 +84,16 @@ function runProgram(){
 
     // turn off event handlers
     $(document).off();
+  }
+
+  function repositionGameItem() {
+    positionX += speedX;
+    positionY += speedY;
+  }
+
+  function redrawGameItem() {
+    $("#walker").css("top", positionX);
+    $("#walker").css("left", positionY);
   }
   
 }
