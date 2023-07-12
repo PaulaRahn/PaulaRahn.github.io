57d25b7a99597013c1ad1ba74000ff7a4e9fb025
PaulaRahn <paula.rahn@apsb.org>
2023-07-12 14:59:29 +0000

    saving image-filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-  applyFilter();
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
   
 
   // do not change the below line of code
@@ -32,7 +34,32 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-function applyFilter() {
+function applyFilter(filterFunction) {
+  for (var r = 0; r < image.length; r++){
+    var row = image[r];
+
+    for(var c = 0; c < row.length; c++){
+      var rgbString = image[r][c];
+
+      if (rgbString !== image[0][0]){
+        applyFilter(decreaseBlue);
+        applyFilter(increaseGreenByBlue);
+      }
+      else{
+        var rgbNumbers = rgbStringToArray(rgbString);
+
+        filterFunction(rgbNumbers);
+
+        rgbString = rgbArrayToString(rgbNumbers);
+      
+        image[r][c] = rgbString;
+      }
+    }
+  }
+}
+
+// TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(){
   for (var r = 0; r < image.length; r++){
     var row = image[r];
 
@@ -41,26 +68,78 @@ function applyFilter() {
 
       var rgbNumbers = rgbStringToArray(rgbString);
 
-      rgbNumbers[red] = 255;
+      //rgbNumbers[RED] = 255;
+      filterFunction(rgbNumbers);
 
       rgbString = rgbArrayToString(rgbNumbers);
       
       image[r][c] = rgbString;
     }
-
   }
 }
 
-// TODO 7: Create the applyFilterNoBackground function
-
-
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(num){
+  return (num < 0 ) ? (0) : ((num > 255) ? (255) : (num));
+}
 
 // TODO 3: Create reddify function
+function reddify(arr) {
+  for (var r = 0; r < image.length; r++){
+    var row = image[r];
 
+    for(var c = 0; c < row.length; c++){
+      var rgbString = image[r][c];
+
+      var rgbNumbers = rgbStringToArray(rgbString);
+
+      rgbNumbers[RED] = 200;
+      
+      
+      rgbString = rgbArrayToString(rgbNumbers);
+      
+      image[r][c] = rgbString;
+    }
+  }
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue(arr1){
+  for (var r = 0; r < image.length; r++){
+    var row = image[r];
 
+    for(var c = 0; c < row.length; c++){
+      var rgbString = image[r][c];
+
+      var rgbNumbers = rgbStringToArray(rgbString);
+
+      rgbNumbers[BLUE] = keepInBounds(rgbNumbers[BLUE] - 50);
+      
+      
+      rgbString = rgbArrayToString(rgbNumbers);
+      
+      image[r][c] = rgbString;
+    }
+  }  
+}
+
+function increaseGreenByBlue(arr2){
+  for (var r = 0; r < image.length; r++){
+    var row = image[r];
+
+    for(var c = 0; c < row.length; c++){
+      var rgbString = image[r][c];
+
+      var rgbNumbers = rgbStringToArray(rgbString);
+
+      rgbNumbers[GREEN] = keepInBounds(rgbNumbers[BLUE] + rgbNumbers[GREEN]);
+      
+      
+      rgbString = rgbArrayToString(rgbNumbers);
+      
+      image[r][c] = rgbString;
+    }
+  }  
+}
 
 // CHALLENGE code goes below here
